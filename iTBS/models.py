c5aaebe385f8c211a50036b3b78345e81717ea56
Lesedi Skosana <111176752+ark-lesedium@users.noreply.github.com>
2024-08-13 11:15:53 +0200

    Update models.py
    Lesedi Skosana Update

--- iTBS/models.py
+++ iTBS/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class info(models.Model):
+  name = models.textField(max_length=100)
+  surname = models.textField(max_length=100)
+  student_number = models.CharField()
+  
+  def __str__:
+    return info.name
